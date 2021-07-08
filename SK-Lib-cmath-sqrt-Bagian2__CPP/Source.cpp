8f152e9f6dcab8e65b4d46945d14446e79c3417c
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-07-08 21:09:20 +0700

    Add project files.

---
+++ SK-Lib-cmath-sqrt-Bagian2__CPP/Source.cpp
@@ -0,0 +1,18 @@
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main() {
+	int x = 67;
+	int y = 50;
+
+	cout << fixed << setprecision(12) << sqrt(x) << endl;
+	cout << fixed << setprecision(12) << sqrt(y) << endl;
+
+	_getch();
+	return (0);
+}
